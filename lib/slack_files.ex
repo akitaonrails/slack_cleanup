1f719b1931fae765ee06e40f1e50df58cde1b30e
Fabio Akita <fabioakita@gmail.com>
2015-10-29 19:39:18 -0200

    Adding separated module to fetch all files from Slack

---
+++ lib/slack_files.ex
@@ -0,0 +1,24 @@
+defmodule SlackFiles do
+  use HTTPoison.Base
+
+  @expected_fields ~w(files)
+
+  def fetch(token) do
+    post!("/api/files.list", params(token)).body[:files]
+  end
+
+  def process_url(url) do
+    "https://slack.com" <> url
+  end
+
+  def process_response_body(body) do
+    body
+    |> Poison.decode!
+    |> Dict.take(@expected_fields)
+    |> Enum.map(fn({k, v}) -> {String.to_atom(k), v} end)
+  end
+
+  defp params(token) do
+    {:form, [token: token, ts_to: :os.system_time(:seconds)]}
+  end
+end
